f1144a8b29a6fb5390326d13cbf3d07c284fc48a
rafcole <39354464+rafcole@users.noreply.github.com>
2023-12-17 13:12:08 -0800

    added course info

---
+++ part1/workthrough/src/App.jsx
@@ -0,0 +1,18 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+export default App
