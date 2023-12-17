2460a9f6ae9668b9f2a68eb1e31d41de45fe7594
rafcole <39354464+rafcole@users.noreply.github.com>
2023-12-17 13:22:38 -0800

    1.2 prop corrections

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,18 +7,21 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
     </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
   )
 }
 
@@ -41,8 +44,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} excercises2={exercises2}
-        part3={part3} excercises3={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
